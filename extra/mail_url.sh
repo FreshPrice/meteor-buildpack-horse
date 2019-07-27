d4748f058ef6d64ade55947aac1542031142a107
Yao Liu <io.yao.liu@gmail.com>
2019-07-27 11:09:42 -0700

    Add Mail url

---
+++ extra/mail_url.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Mail URL for mail sending
+#
+echo "-----> Adding profile script to resolve MAIL_URL from config"
+cat > "$APP_CHECKOUT_DIR"/.profile.d/mail_url.sh <<EOF
+  #!/bin/bash
+  if [ -z "\$MAIL_URL" ] ; then
+    export MAIL_URL="smtps://freshpricefresh%40gmail.com:FreshPrice2019@smtp.gmail.com:465/"
+  fi
+EOF
